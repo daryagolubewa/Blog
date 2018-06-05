02f1abd69b7bea6d5f87bf5f86b72efc7ed1dd05
daryagolubewa <darya.golubewa@gmail.com>
2018-06-05 18:27:06 +0300

    FormPost is updated;

--- src/Containers/FormPost.js
+++ src/Containers/FormPost.js
@@ -12,27 +12,19 @@ const AddPost = ({ dispatch }) => {
             <form
                 onSubmit={e => {
                     e.preventDefault()
-                    if (!userName.value.trim()) {
+                    if (!userName.value.trim() || !title.value.trim() || !text.value.trim()) {
                         return
                     }
-                    if (!title.value.trim()) {
-                        return
-                    }
-                    if (!text.value.trim()) {
-                        return
-                    }
-                    dispatch(addPost(userName.value))
+                    dispatch(addPost(userName.value, title.value, text.value))
                     userName.value = '';
-                    dispatch(addPost(title.value))
                     title.value = '';
-                    dispatch(addPost(text.value))
                     text.value = '';
                 }}
 
             >
-                <input ref={node => userName = node}  />
-                <input ref={node => title = node}  />
-                <input ref={node => text = node}  />
+                <input ref={node => userName = node} placeholder="Введите ваше имя" />
+                <input ref={node => title = node} placeholder="Введите заголовок" />
+                <textarea ref={node => text = node} placeholder="Введите текст" />
                 <button type="submit">
                     Отправить
                 </button>
